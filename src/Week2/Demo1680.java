c7ae687095a3c1cbaac8b1048f50cb6e9cb2b8f8
l98fo <198forr@gmail.com>
2022-05-17 15:07:41 +0800

    1

---
+++ src/Week2/Demo1680.java
@@ -0,0 +1,16 @@
+package Week2;
+
+import java.util.Scanner;
+
+public class Demo1680 {
+    public static void main(String[] args) {
+        System.out.println("请输入数字n：");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+
+    }
+    public static String getTwo(int n){
+    return " ";
+    }
+}
---
+++ src/greedyalgorithm/Demo1.java
@@ -0,0 +1,4 @@
+package greedyalgorithm;
+
+public class Demo1 {
+}
