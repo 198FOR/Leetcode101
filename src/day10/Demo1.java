6e7bfeadf25cc6735b2759eff75015a0853d6e87
clkk <45221342+198FOR@users.noreply.github.com>
2022-05-18 09:26:17 +0800

    day10

--- src/day10/Demo1.java
+++ src/day10/Demo1.java
@@ -1,4 +1,12 @@
 package day10;
 
+import static java.lang.Math.random;
+
 public class Demo1 {
+    public static void main(String[] args) {
+        int a = (int) random();
+        System.out.println(a);
+        int b = (int) random();
+        System.out.println(b);
+    }
 }
